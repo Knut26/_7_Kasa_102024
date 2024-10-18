f61caa4154e03569df45462d2c11a279af25b834
Olivier <herculepoirot75@hotmail.fr>
2024-10-18 11:42:23 +0200

    css margin auto oké

--- src/assets/jsx_components/banner.jsx
+++ src/assets/jsx_components/banner.jsx
@@ -3,9 +3,7 @@ import React from "react";
 function Banner() {
   return (
     <div className="banner">
-      <div className="banner__container">
-        <div className="banner__text">Chez vous, partout et ailleurs</div>
-      </div>
+      <div className="banner__text">Chez vous, partout et ailleurs</div>
     </div>
   );
 }
