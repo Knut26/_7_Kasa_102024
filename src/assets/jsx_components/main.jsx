a15f98d85f0120f69ffadae2a5a6a144ca667586
Olivier <herculepoirot75@hotmail.fr>
2024-10-21 14:09:15 +0200

    react router oké

--- src/assets/jsx_components/main.jsx
+++ src/assets/jsx_components/main.jsx
@@ -2,11 +2,19 @@ import { StrictMode } from "react";
 //import { createRoot } from "react-dom/client";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.jsx";
 import "../css_components/app.scss";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
