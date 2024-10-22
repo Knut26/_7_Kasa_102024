420d68ea355766aad85f4e3a23c2799d9109d7a8
Olivier <herculepoirot75@hotmail.fr>
2024-10-22 16:51:02 +0200

    collapses

--- src/assets/jsx_components/Apparts.jsx
+++ src/assets/jsx_components/Apparts.jsx
@@ -61,11 +61,11 @@ function Apparts() {
           <div className="canal">Canal</div>
           <div className="paris10">Paris 10</div>
         </div>
-        <div className="collapse1__and__2">
-          <div className="collapse1">
-            <div className="collapse1__contentBox">
-              <div className="collapse1__label">Description</div>
-              <div className="collapse1__content">
+        <div className="apparts__collapse1__and__2">
+          <div className="apparts__collapse1">
+            <div className="apparts__collapse1__contentBox">
+              <div className="apparts__collapse1__label">Description</div>
+              <div className="apparts__collapse1__content">
                 <p>
                   Vous serez à 50m du canal Saint-Martin où vous pourrez
                   pique-niquer l'été et à côté de nombreux bars et restaurants.
@@ -77,10 +77,10 @@ function Apparts() {
               </div>
             </div>
           </div>
-          <div className="collapse2">
-            <div className="collapse2__contentBox">
-              <div className="collapse2__label">Equipements</div>
-              <div className="collapse2__content">
+          <div className="apparts__collapse2">
+            <div className="apparts__collapse2__contentBox">
+              <div className="apparts__collapse2__label">Equipements</div>
+              <div className="apparts__collapse2__content">
                 <p>
                   <ul>
                     <li>Climatisation</li>
@@ -102,19 +102,19 @@ function Apparts() {
   );
 }
 
-const collapse1 = document.getElementByClassName("collapse1__contentBox");
+// const collapse1 = document.getElementByClassName("collapse1__contentBox");
 
-for (let i = 0; i < collapse1.length; i++) {
-  collapse1[i].addEventListener("click", () => {
-    this.classList.toggle("active");
-  });
-}
-const collapse2 = document.getElementByClassName("collapse2__contentBox");
+// for (let i = 0; i < collapse1.length; i++) {
+//   collapse1[i].addEventListener("click", () => {
+//     this.classList.toggle("active");
+//   });
+// }
+// const collapse2 = document.getElementByClassName("collapse2__contentBox");
 
-for (let i = 0; i < collapse2.length; i++) {
-  collapse2[i].addEventListener("click", () => {
-    this.classList.toggle("active");
-  });
-}
+// for (let i = 0; i < collapse2.length; i++) {
+//   collapse2[i].addEventListener("click", () => {
+//     this.classList.toggle("active");
+//   });
+// }
 
 export default Apparts;
