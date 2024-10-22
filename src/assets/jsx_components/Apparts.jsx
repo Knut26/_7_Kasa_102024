42cdef2d26961dc1e4725bc29fc73b08259ff1e8
Olivier <herculepoirot75@hotmail.fr>
2024-10-22 14:37:20 +0200

    css suite apparts.jsx

--- src/assets/jsx_components/Apparts.jsx
+++ src/assets/jsx_components/Apparts.jsx
@@ -19,44 +19,50 @@ function Apparts() {
           />
         </div>
         <div className="apparts__title__and__owner">
-          <h1 className="apparts__title">Cozy loft on Canal Saint-Martin</h1>
-          <h4 className="apparts__owner">Alexandre Dumas</h4>
-          <div className="apparts__owner__picture"></div>
-        </div>
-        <p className="apparts__subtitle">Paris, Île-de-France</p>
-        <div className="apparts__owner__tag__and__ratings">
-          <div className="apparts__owner__tag">
-            <div className="cosy">Cosy</div>
-            <div className="canal">Canal</div>
-            <div className="paris10">Paris 10</div>
+          <div className="apparts__title__subtitle">
+            <h1 className="apparts__title">Cozy loft on Canal Saint-Martin</h1>
+            <p className="apparts__subtitle">Paris, Île-de-France</p>
           </div>
           <div className="apparts__owner__rating">
-            <FontAwesomeIcon
-              icon={faStarSolid}
-              size="xl"
-              className="apparts__owner__rating__stars"
-            />
-            <FontAwesomeIcon
-              icon={faStarSolid}
-              size="xl"
-              className="apparts__owner__rating__stars"
-            />
-            <FontAwesomeIcon
-              icon={faStarSolid}
-              size="xl"
-              className="apparts__owner__rating__stars"
-            />
-            <FontAwesomeIcon
-              icon={faStarRegular}
-              size="xl"
-              className="apparts__owner__rating__stars"
-            />
-            <FontAwesomeIcon
-              icon={faStarRegular}
-              size="xl"
-              className="apparts__owner__rating__stars"
-            />
+            <div className="apparts__owner__and__rating">
+              <div className="apparts__owner_and__picture">
+                <h4 className="apparts__owner">Alexandre Dumas</h4>
+                <div className="apparts__owner__picture"></div>
+              </div>
+              <div className="apparts__owner__and__rating__itself">
+                <FontAwesomeIcon
+                  icon={faStarSolid}
+                  size="xl"
+                  className="apparts__owner__rating__stars"
+                />
+                <FontAwesomeIcon
+                  icon={faStarSolid}
+                  size="xl"
+                  className="apparts__owner__rating__stars"
+                />
+                <FontAwesomeIcon
+                  icon={faStarSolid}
+                  size="xl"
+                  className="apparts__owner__rating__stars"
+                />
+                <FontAwesomeIcon
+                  icon={faStarRegular}
+                  size="xl"
+                  className="apparts__owner__rating__stars"
+                />
+                <FontAwesomeIcon
+                  icon={faStarRegular}
+                  size="xl"
+                  className="apparts__owner__rating__stars"
+                />
+              </div>
+            </div>
           </div>
+        </div>
+        <div className="apparts__owner__tag">
+          <div className="cosy">Cosy</div>
+          <div className="canal">Canal</div>
+          <div className="paris10">Paris 10</div>
         </div>
       </div>
       <Footer />
