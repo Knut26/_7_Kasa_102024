c4dedc3baa6b2fec65a9b013837146324bd869ad
Olivier <herculepoirot75@hotmail.fr>
2024-10-22 11:37:24 +0200

    test import font awesome

--- src/assets/jsx_components/Apparts.jsx
+++ src/assets/jsx_components/Apparts.jsx
@@ -6,7 +6,24 @@ function Apparts() {
   return (
     <div>
       <Navbar />
-      <h1>test apparts</h1>;
+      <div className="apparts__main">
+        <img
+          className="navbar__logo__kasa"
+          src="src/images/landscape.png"
+          alt="Logo Kasa"
+        />
+        <h1 className="apparts__title">Cozy loft on Canal Saint-Martin</h1>
+        <h3 className="apparts__subtitle">Paris, Île-de-France</h3>
+        <h4 className="apparts__owner">Alexandre Dumas</h4>
+        <div className="apparts__owner__picture"></div>
+        <div className="apparts__owner__rating">
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-regular fa-star"></i>
+          <i class="fa-regular fa-star"></i>
+        </div>
+      </div>
       <Footer />
     </div>
   );
