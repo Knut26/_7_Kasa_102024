f8fd6c0d0561c19ce499417e611131c7a52ee459
Olivier <herculepoirot75@hotmail.fr>
2024-10-24 14:59:07 +0200

    fetch start

--- src/assets/jsx_components/cards.jsx
+++ src/assets/jsx_components/cards.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import { Outlet, Link } from "react-router-dom";
 
 function Cards() {
+  fetch("src/images/appartements.json")
+    .then((res) => res.json())
+    .then((response) => console.log(response))
+    .catch(console.error);
+
   return (
     <div className="cards__container">
       <div className="card">
